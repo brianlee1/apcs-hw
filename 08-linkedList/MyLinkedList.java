87b0c9341ea11d9e3354fade49f1fdcfb82ad384
Brian Lee <brianlee1@users.noreply.github.com>
2014-03-20 08:28:55 -0400

    8

--- 08-linkedList/MyLinkedList.java
+++ 08-linkedList/MyLinkedList.java
@@ -14,32 +14,61 @@ public class MyLinkedList{
     
     public String toString(){
 	String s;
-	s = " " + head + head.getNext();
+	Node temp = head;
+	while (temp.getNext() != null){
+	    s = s + temp;
+	    temp = temp.getNext();
+	}
 	return s;
     }
 
     public void add(int i, String s){
 	Node addTemp = new Node(s);
-	while (i > 0){
-	    
-		
+	Node temp = head;
+	for (int j = 0; j < i; j++){
+	    temp = temp.getNext();
+	}
+	addTemp.setNext(temp.getNext());
+	temp.setNext(addTemp);
+	head = temp;
     }
 
     public String get(int i){
-	while (i > 0){
+	Node temp = head;
+	for (int j = 0; j < i; j++){
+	    temp = temp.getNext();
+	}
+	return temp;
 	    
     }
 
     public String set(int i, String s){
-	
+	Node temp = head;
+	for (int j < 0; j < i; j++){
+	    temp = temp.getNext();
+	}
+	String x = temp.getData();
+	temp.setData(s);
+	return x;
     }
 
     public String remove(int i){
-	
+	Node temp = head;
+	for (int j < 0; j < i - 1; j++){
+	    temp = temp.getNext();
+	}
+	String x = temp.getNext().getData();
+	temp.setNext(temp.getNext().getNext());
+	return x;	
     }
 
     public int find(String s){
-	
+	for (int i = 0; i < length(); i++){
+	    if (temp.getData().equals(s))
+		return i;
+	    temp = temp.getNext();
+	}
+	return -1;
     }
 
     public int length(){
