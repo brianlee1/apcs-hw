c84e0a2d1623e7ba6759962877cf93fb3f404f32
brianlee1 <dadude.bl@gmail.com>
2014-04-08 01:07:26 -0400

    13

--- 13-calc/rpn.java
+++ 13-calc/rpn.java
@@ -1,40 +1,40 @@
 import java.util.Scanner;
 
 public class rpn{
-  private myStack stack = new myStack();
+    private myStack stack = new myStack();
   
-  public void push(String s){
-    double x = Double.parseDouble(s);
-    stack.push(x);
-  }
-  
-  public void add(){
-    if (stack.getSize() < 2){
-      return;
+    public void push(String s){
+	double x = Double.parseDouble(s);
+	stack.push(x);
     }
-    stack.push(stack.pop() + stack.pop());
-  }
   
-  public void subtract(){
-    if (stack.size() < 2){
-      return;
+    public void add(){
+	if (stack.getSize() < 2){
+	    return;
+	}
+	stack.push(stack.pop() + stack.pop());
     }
-    stack.push(stack.pop() - stack.pop());
-  }
   
-  public void multiply(){
-    if (stack.size() < 2){
-      return;
+    public void subtract(){
+	if (stack.size() < 2){
+	    return;
+	}
+	stack.push(stack.pop() - stack.pop());
     }
-    stack.push(stack.pop() * stack.pop());
-  }
   
-  public void divide(){
-    if (stack.size() < 2){
-      return;
+    public void multiply(){
+	if (stack.size() < 2){
+	    return;
+	}
+	stack.push(stack.pop() * stack.pop());
     }
-    stack.push(stack.pop() / stack.pop());
-  }
   
-  //will do main function later
+    public void divide(){
+	if (stack.size() < 2){
+	    return;
+	}
+	stack.push(stack.pop() / stack.pop());
+    }
+  
+    //will do main function later
 }
