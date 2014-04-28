7cdfb7908e638cf3f50c9ffce0a169c890aed662
brianlee1 <dadude.bl@gmail.com>
2014-04-28 00:13:52 -0400

    15

--- 15-tree/bst.java
+++ 15-tree/bst.java
@@ -1,16 +1,50 @@
 public class bst{
     private treeNode root;
 
+	public bst(){
+		root = null;
+	}
+	
     public treeNode search(int x){
 	treeNode p = root;
-	while (p != null && p.getData() != x){
-	    if (p.getData() > x){
-		p = p.getLeft();
-	    }
-	    else 
-		p = p.getRight();
+	while (p != null){
+		if (x > p.getData()){
+			p = p.getRight();
+		}
+		else if(x < p.getData()){
+			p = p.getLeft();
+		}
+		else{
+			return p;
+		}
 	}
-	return p;
+	return null;
+    }
+    
+    public void insert(int x){
+    	if (root == null){
+    		root = new treeNode(x);
+    		return;
+    	}
+    	treeNode p = root;
+    	treeNode q = null;
+    	while (p != null){
+    		if (x < p.getData()){
+    			q = p;
+    			p = p.getLeft();
+    	    	}
+    	    	else{
+    	    		q = p;
+    	    		p = p.getRight();
+    	    	}
+    	    	
+    	}
+    	if (x < q.getData()){
+    		q.setLeft(new treeNode(x));
+    	}
+    	else{
+    		q.setRight(new treeNode(x));
+    	}
     }
 }
        
