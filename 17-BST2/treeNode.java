037affb1f3e79855cd8715125418ad05d174547d
Brian Lee <brianlee1@users.noreply.github.com>
2014-04-29 07:59:50 -0400

    incomplete

---
+++ 17-BST2/treeNode.java
@@ -0,0 +1,38 @@
+public class treeNode{
+    private int data;
+    private treeNode left, right;
+    
+    public treeNode(int i){
+	data = i;
+	right = null;
+	left = null;
+    }
+
+    public void setData(int i){
+	data = i;
+    }
+
+    public int getData(){
+	return data;
+    }
+
+    public void setRight(Node n){
+	right = n;
+    }
+
+    public void setLeft(Node n){
+	left = n;
+    }
+
+    public void getRight(){
+	return right;
+    }
+
+    public void getLeft(){
+	return left;
+    }
+
+    public String toString(){
+	return "" + String.valueOf(data);
+    }
+}
